73cee19c493477b4704ecccf8923c274356228cd
Elliot Blanchard <git@elliotblanchard.com>
2022-02-08 16:44:04 -0500

    Set up initial routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    return <p>I am the Navbar</p>
+}
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const SearchPage = () => {
+
+    return <p>I am the Search Page</p>
+}
